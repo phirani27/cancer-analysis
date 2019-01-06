061a2249546bdd9551c9d1e4996575d56eb70735
phirani27 <pritesh.hirani@pens.com>
2019-01-07 01:00:03 +0530

    scatter chart

--- report.js
+++ report.js
@@ -111,42 +111,20 @@ function makeViolinChart(){
     Plotly.plot('violin-chart', data, layout);    
 
 
-};
+}
 
-function makeScatterChart(){
+function makeScatterChart(){ 
     // from http://bl.ocks.org/mbostock/4349187
     // Sample from a normal distribution with mean 0, stddev 1.
 
-    function normal() {
-        var x = 0,
-            y = 0,
-            rds, c;
-        do {
-            x = Math.random() * 2 - 1;
-            y = Math.random() * 2 - 1;
-            rds = x * x + y * y;
-        } while (rds == 0 || rds > 1);
-        c = Math.sqrt(-2 * Math.log(rds) / rds); // Box-Muller transform
-        return x * c; // throw away extra sample y * c
-    }
-
-    var N = 2000,
-    a = -1,
-    b = 1.2;
-
-    var step = (b - a) / (N - 1);
-    var t = new Array(N), x = new Array(N), y = new Array(N);
-
-    for(var i = 0; i < N; i++){
-        t[i] = a + step * i;
-        x[i] = (Math.pow(t[i], 3)) + (0.3 * normal() );
-        y[i] = (Math.pow(t[i], 6)) + (3 * normal() );
-    }
-
-    console.log(t);
-    console.log(x);
-    console.log(y);
-
+    //x = data from first.. 
+    //y = data from second..
+    let xCord = $('#scatter1').val();
+    let yCord = $('#scatter2').val();
+    let x = ANALYSIS_DATA.map((attr)=>attr[xCord]);
+    let y = ANALYSIS_DATA.map((attr)=>attr[yCord]);
+    console.log(xCord);
+    console.log(yCord);
     var trace1 = {
         x: x,
         y: y,
@@ -227,7 +205,7 @@ $('#violin-select').multipleSelect({
     }
 });
 
-$("#scatter-dropdown").change(function(){
+$(".scatter-dropdown").change(function(){
     Plotly.purge('scatter-chart');
     makeScatterChart();    
 }); 
@@ -235,3 +213,5 @@ $("#scatter-dropdown").change(function(){
 makeViolinChart();
 
 makeScatterChart();
+
+//makeSplomChart();
